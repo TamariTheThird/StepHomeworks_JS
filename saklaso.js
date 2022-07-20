8b36944910eec992180b0614ed8e720abd0ba36d
TamariTheThird <tamari.bablidze@gmail.com>
2022-07-20 22:12:46 +0400

    js

--- saklaso.js
+++ saklaso.js
@@ -1,21 +1,14 @@
 let decrease = document.querySelector('.decrease');
-
 let resete = document.querySelector('.resete');
-
 let increase = document.querySelector('.increase');
-
 let count = 0;
-
 let update = document.querySelector('#input');
-
 increase.addEventListener('click', function () {
   update.value++;
 });
-
 decrease.addEventListener('click', function () {
   update.value--;
 });
-
 resete.addEventListener('click', function () {
   update.value = 0;
 });
